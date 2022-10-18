33b463ceb99068132e616fbe03fe5a8b2fa3ec18
Dawid Matras <davematras@gmail.com>
2022-10-18 22:37:04 +0200

    next lesson

--- Intro/sandbox.js
+++ Intro/sandbox.js
@@ -69,3 +69,24 @@ for (let i = 0; i < c.length; i++) {
     break;
   }
 }
+// ***** NEX Lesson ******* //
+let d = "B";
+switch (d) {
+  case "A":
+    console.log("you got an A");
+    break;
+  case "B":
+    console.log("you got an B");
+    break;
+  case "C":
+    console.log("you got an C");
+    break;
+  case "D":
+    console.log("you got an D");
+    break;
+  case "E":
+    console.log("you got an E");
+    break;
+  default:
+    console.log("you got nothing good");
+}
