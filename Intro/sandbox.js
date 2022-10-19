e50acba73fbfda19768191a312b8d4646df7cbfa
Dawid Matras <davematras@gmail.com>
2022-10-19 20:31:33 +0200

    objects

--- Intro/sandbox.js
+++ Intro/sandbox.js
@@ -98,3 +98,29 @@ switch (d) {
 const calcArea = (radius) => 3.14 * radius ** 2;
 const area = calcArea(5);
 console.log(area);
+
+const greet = function () {
+  return "hello world";
+};
+const greet2 = () => "hello world";
+
+const bill = function (products, tax) {
+  let total = 0;
+  for (let i = 0; i < products.length; i++) {
+    total += products[i] + products[i] + tax;
+  }
+  return total;
+};
+const bill2 = (products, tax) => {
+  let total = 0;
+  for (let i = 0; i < products.length; i++) {
+    total += products[i] + products[i] + tax;
+  }
+  return total;
+};
+
+let people = ["mario", "ted", "ken", "lee"];
+
+people.forEach((fred, friend) => {
+  console.log(fred, friend);
+});
